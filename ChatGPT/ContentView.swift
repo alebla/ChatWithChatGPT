84a3b3224d37ab12c6bb5c24c7848cff0a9ef6ae
aB <self.alexb@gmail.com>
2023-01-04 11:10:05 -0600

    Integrated OpenAISwift. Simple chat with ai implemented

--- ChatGPT/ContentView.swift
+++ ChatGPT/ContentView.swift
@@ -18,19 +18,68 @@ final class ViewModel: ObservableObject {
     }
     
     func send(text: String, completion: @escaping (String) -> Void) {
-        
+        client?.sendCompletion(with: text, maxTokens: 2048 ) { result in
+            switch result {
+                case .success(let model):
+                    let output = model.choices.first?.text ?? ""
+                    completion(output)
+                case .failure(let error):
+                    print(error.localizedDescription)
+            }
+            
+        }
     }
 }
 
 struct ContentView: View {
+    @ObservedObject var viewModel = ViewModel()
+    @State private var questionText = ""
+    @State private var conversations = [""]
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(alignment: .leading) {
+            ScrollView {
+                ForEach(conversations, id: \.self) { conversation in
+                    Text(conversation)
+                }
+            }
+            
+            Spacer()
+            
+            HStack {
+                TextField("Ask ChatGpt a question...", text: $questionText)
+                    .padding()
+                    .background(.thinMaterial)
+                    .clipShape(Capsule())
+                Button {
+                    send()
+                } label: {
+                    Text("Send")
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(.blue)
+                        .clipShape(Capsule())
+                }
+            }
+            .padding([.bottom, .top])
+            .onAppear(perform: viewModel.setup)
         }
         .padding()
+    }
+    
+    func send() {
+        guard !questionText.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty else { return }
+        
+        conversations.append("Me: \(questionText)")
+        
+        viewModel.send(text: questionText) { response in
+            DispatchQueue.main.async {
+                self.conversations.append("ChatGPT: " + response)
+                questionText = ""
+            }
+        }
+        questionText = ""
     }
 }
 
